08c29df85c909698f2743a4802fdd391e2a243d2
PRAKALP-PANDE <prakalpsp16@gmail.com>
2024-02-05 20:09:39 +0530

    Initial App Layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Home from './components/Home';
+import Loader from './components/Loader';
+import Navbar from './components/Navbar';
 
 function App() {
+  // Loader
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating a delay to mimic data fetching or other initialization
+    const delay = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    // Cleanup the timeout on component unmount
+    return () => clearTimeout(delay);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isLoading ? (
+        // Loading spinner or message
+        <div><Loader /></div>
+      ) : (
+        <div className="grid grid-cols-12 h-screen">
+          <div>
+            <Navbar />
+          </div>
+          <div className="col-span-11 p-4">
+            <Home />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
