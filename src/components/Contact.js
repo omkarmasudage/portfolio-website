43212816ff65579a0e15349f555c0ca048807b68
PRAKALP-PANDE <prakalpsp16@gmail.com>
2024-02-09 01:44:32 +0530

    Contact page updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -131,11 +131,11 @@ const Contact = () => {
                                 <button className="flex mx-auto text-white bg-[#622c00] border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" type="submit" >Button</button>
                             </div>
                             <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-                                <a href="/" className="text-indigo-500">parthkumbhar269@gmail.com</a>
+                                <span href="/" className="text-indigo-500">parthkumbhar269@gmail.com</span>
                                 <p className="leading-normal my-5">49 Smith St.
                                     <br />Saint Cloud, MN 56301
                                 </p>
-                                <span className="inline-flex">
+                                {/* <span className="inline-flex">
                                     <a href="/" className="text-gray-500">
                                         <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                                             <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
@@ -157,7 +157,7 @@ const Contact = () => {
                                             <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                                         </svg>
                                     </a>
-                                </span>
+                                </span> */}
                             </div>
                         </div>
                     </div>
