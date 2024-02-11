2cb2483f9fdbc963d5bf9b58963d7ad0a059e84a
PRAKALP-PANDE <prakalpsp16@gmail.com>
2024-02-12 02:29:36 +0530

    Added toast

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import sheetdb from 'sheetdb-node';
 // import axios from 'axios'
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Contact = () => {
@@ -18,8 +18,8 @@ const Contact = () => {
         // Extract form data
         const formData = `Name: ${name}\nLast Name: ${lname}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`;
         const Contactdata = [{
-            Message : formData,
-        }] 
+            Message: formData,
+        }]
 
 
         const config = {
@@ -33,9 +33,34 @@ const Contact = () => {
         client.create(Contactdata)
             .then((response) => {
                 console.log('Data stored in sheetdb:', response);
+                setName('');
+                setLname('');
+                setEmail('');
+                setPhone('');
+                setMessage('');
+                toast.success('Form submitted successfully', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
             })
             .catch((error) => {
                 console.error('Error storing data in sheetdb:', error);
+                toast.error('Something went wrong...!', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
             });
 
     };
@@ -70,24 +95,24 @@ const Contact = () => {
 
     return (
         <div className="text-black body-font relative">
-            <ToastContainer
-                position="top-right"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-            />
             <div className="container px-5 py-8 mx-auto">
                 <div className="h-1 bg-gray-200 rounded overflow-hidden mb-6">
                     <div className="w-24 h-full bg-[#622c00]"></div>
                 </div>
                 <div className="flex flex-col text-center w-full mb-12">
                     <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-[#622c00]">Contact Us</h1>
+                    <ToastContainer
+                        position="top-right"
+                        autoClose={5000}
+                        hideProgressBar={false}
+                        newestOnTop={false}
+                        closeOnClick
+                        rtl={false}
+                        pauseOnFocusLoss
+                        draggable
+                        pauseOnHover
+                        theme="light"
+                    />
                     {/* <p className="lg:w-2/3 mx-auto leading-relaxed text-[#622c00]">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p> */}
                 </div>
                 <form onSubmit={handleSubmit}>
@@ -96,35 +121,35 @@ const Contact = () => {
                             <div className="p-2 w-1/2">
                                 <div className="relative">
                                     <label className="leading-7 text-sm text-black">First Name*</label>
-                                    <input type="text" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={(e) => setName(e.target.value)} value={name} required />
+                                    <input type="text" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-[#622c00] focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={(e) => setName(e.target.value)} value={name} required />
                                 </div>
                             </div>
                             <div className="p-2 w-1/2">
                                 <div className="relative">
                                     <label className="leading-7 text-sm text-black">Last Name*</label>
-                                    <input type="text" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={(e) => setLname(e.target.value)} value={lname} required />
+                                    <input type="text" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-[#622c00] focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={(e) => setLname(e.target.value)} value={lname} required />
                                 </div>
                             </div>
                             <div className="p-2 w-1/2">
                                 <div className="relative">
                                     <label className="leading-7 text-sm text-black">Company Email*</label>
-                                    <input type="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={(e) => setEmail(e.target.value)} value={email} required />
+                                    <input type="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-[#622c00] focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={(e) => setEmail(e.target.value)} value={email} required />
                                 </div>
                             </div>
                             <div className="p-2 w-1/2">
                                 <div className="relative">
                                     <label className="leading-7 text-sm text-black">Phone Number*</label>
-                                    <input type="phone" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={(e) => setPhone(e.target.value)} value={phone} required />
+                                    <input type="phone" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-[#622c00] focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={(e) => setPhone(e.target.value)} value={phone} required />
                                 </div>
                             </div>
                             <div className="p-2 w-full">
                                 <div className="relative">
                                     <label className="leading-7 text-sm text-black">Message*</label>
-                                    <textarea className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" onChange={(e) => setMessage(e.target.value)} value={message} required></textarea>
+                                    <textarea className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-[#622c00] focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" onChange={(e) => setMessage(e.target.value)} value={message} required></textarea>
                                 </div>
                             </div>
                             <div className="p-2 w-full">
-                                <button className="flex mx-auto text-[#622c00] bg-[#f1d29c] border-[#622c00] border-2 py-2 px-8 focus:outline-none hover:bg-[#622c00] hover:text-white rounded text-lg" type="submit" >Button</button>
+                                <button className="flex mx-auto text-[#622c00] bg-[#f1d29c] border-[#622c00] border-2 py-2 px-8 focus:outline-none hover:bg-[#622c00] hover:text-white rounded text-lg" type="submit" >Submit</button>
                             </div>
                             <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
                                 <span href="/" className="text-[#622c00]">parthkumbhar269@gmail.com</span>
@@ -142,7 +167,7 @@ const Contact = () => {
                                             <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                                         </svg>
                                     </a> */}
-                                    <a href="https://www.instagram.com/pk_design.studio" className="ml-4 text-[#622c00]">
+                                    <a href="https://www.instagram.com/pk_design.studio" target='_blank' className="ml-4 text-[#622c00]">
                                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                                             <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                                             <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
